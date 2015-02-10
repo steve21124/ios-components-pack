6b2b50369e62a36080d39f01f8841b31582fae09
Arthur Igberdin <art.igberdin@gmail.com>
2015-02-10 11:19:51 +0300

    Implement Custom Camera - capture video, a take photo

---
+++ CustomCamera/CustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCamera
+//
+//  Created by Artur on 09/02/15.
+//  Copyright (c) 2015 Artur Igberdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
