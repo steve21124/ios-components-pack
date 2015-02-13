3ef7571a0f84d136dce8ed7eef34bc4771583b17
Arthur Igberdin <art.igberdin@gmail.com>
2015-02-14 01:18:05 +0300

    Reveal Controller - slide out menu

---
+++ SWRevealController-Test/README.md
@@ -0,0 +1,21 @@
+# SWRevealController-Test
+
+Custom segues from controller (SWRevealController) to `sw_rear` and `sw_front` segue.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SWRevealController-Test/SWRevealController-Test/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SWRevealController-Test
+//
+//  Created by Artur on 13/02/15.
+//  Copyright (c) 2015 Artur Igberdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *barButton;
+
+@end
+
