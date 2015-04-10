2a8d5eff209d456a45c6b2a31d9e00b7a97eb672
Arthur Igberdin <art.igberdin@gmail.com>
2015-04-10 14:56:26 +0300

    Ass SwiftObjcCombine project

---
+++ [ Swift ]/SwiftObjcCombine/museum-swift/HomeController.h
@@ -0,0 +1,13 @@
+//
+//  HomeController.h
+//  museum-swift
+//
+//  Created by Artur on 10/04/15.
+//  Copyright (c) 2015 Artur Igberdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeController : UIViewController
+
+@end
---
+++ [ Swift ]/SwiftObjcCombine/museum-swift/HomeController.m
@@ -0,0 +1,13 @@
+//
+//  HomeController.m
+//  museum-swift
+//
+//  Created by Artur on 10/04/15.
+//  Copyright (c) 2015 Artur Igberdin. All rights reserved.
+//
+
+#import "HomeController.h"
+
+@implementation HomeController
+
+@end
